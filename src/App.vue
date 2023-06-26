d45c95223f15ec07e9a29bd7007cddfc7b420903
Nguyễn Hữu Thuật <thuatnguyen09112002@gmail.com>
2023-06-26 23:54:13 +0700

    upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from "vue-router";
+import SiteFooter from "./components/SiteFooter.vue";
+import SiteNavbar from "./components/SiteNavbar.vue";
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <site-navbar />
+    <main class="flex-grow">
+      <RouterView />
+    </main>
+    <site-footer />
+  </div>
+</template>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="bg-gray-200 text-center lg:text-left">
+      <div
+        class="text-gray-700 text-center p-4"
+        style="background-color: rgba(0, 0, 0, 0.2)"
+      >
+        © 2023 Copyright:
+        <a class="text-gray-800" href="https://facebook.com/huuthuatnguyen.0911">Nguyễn Hữu Thuật</a>
+      </div>
+    </footer>
+  </template>
